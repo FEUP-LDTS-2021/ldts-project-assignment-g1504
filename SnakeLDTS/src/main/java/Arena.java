ff0cb63d135a2ea2d722155d7093ee16e6c828fa
Freirex101 <guilodinho@gmail.com>
2022-01-19 10:21:52 +0000

    Snake Mexe-se sozinha

--- SnakeLDTS/src/main/java/Arena.java
+++ SnakeLDTS/src/main/java/Arena.java
@@ -1,50 +1,158 @@
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.*;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.security.Key;
 import java.util.LinkedList;
 import java.util.Random;
 
+
 public class Arena {
-    public Arena(int width,int height){
+    private int width,height;
+    static int SCORE;
+    public static final int TICKS_PER_SECOND = 10;
+    private Apple apple;
+    private LinkedList<Boxes> boxes = new LinkedList<Boxes>();
+    private Snake snake;
+
+
+    public Arena(int width,int height, Snake snake){
         this.width = width;
         this.height = height;
-        snake = new Snake(30,15,Direction.RIGHT);
+        this.snake = snake;
         createApples();
         createBoxes();
 
     }
-    public void paintArena(TextGraphics graphics){
-
-        graphics.setBackgroundColor(TextColor.Factory.fromString("#faf5f5"));
+    public void paintArena(Screen screen) throws IOException {
+        TextGraphics graphics = screen.newTextGraphics();
+        graphics.setBackgroundColor(TextColor.ANSI.BLACK_BRIGHT);
         graphics.fillRectangle(new TerminalPosition(0,0),new TerminalSize(width,height), ' ');
         snake.draw(graphics,snake.getColor());
-        apples.get(apples.size()-1).draw(graphics,null);
+        apple.draw(graphics,null);
         for(Boxes b: boxes){
             b.draw(graphics,null);
         }
+        for (int j=0; j<24; j++) {
+            graphics.setBackgroundColor(TextColor.ANSI.WHITE);
+            graphics.putString(0,j," ");
+        }
 
+        for (int j=0; j<24; j++) {
+            graphics.putString(60,j," ");
+        }
+
+        for (int j=0; j<60; j++) {
+            graphics.putString(j,0," ");
+        }
+
+        for (int j=0; j<60; j++) {
+            graphics.putString(j,23," ");
+        }
+
+
+        for (int j=61; j<80; j++) {
+            for (int i = 0; i < 24; i++) {
+                graphics.setBackgroundColor(TextColor.ANSI.WHITE);
+                graphics.putString(j,i," ");
+            }
+        }
+
+        graphics.setBackgroundColor(TextColor.ANSI.WHITE);
+        graphics.setForegroundColor(TextColor.ANSI.BLACK_BRIGHT);
+
+        char c = Symbols.BLOCK_SOLID;
+        graphics.putString( 63, 9, "    " + c + c + c + c + c);
+        graphics.putString( 63, 10, "   " + c + "     " + c);
+        graphics.putString( 63, 11, "   " + c + "      " + c);
+        graphics.putString( 63, 12, "  " + c + " " + c + "   " + c + " " + c);
+        graphics.putString( 63, 13, "  " + c + " " + c + "   " + c + "  " + c);
+        graphics.putString( 63, 14, "   " + c + "      " + c);
+        graphics.putString( 63, 15, "    " + c + c + c + c + c + c + c);
+        graphics.putString( 63, 16, "  " + c + c + "  " + c + "   " + c + c);
+        graphics.putString( 63, 17, " " + c + "   " + c + "    " + c + " " + c);
+        graphics.putString(63, 18, c + "  " + c + "     " + c + "   " + c);
+        graphics.putString(63, 19, c + "   " + c + c + c + c + c + "   " + c + " " + c);
+        graphics.putString(63, 20, " " + c + "         " + c + c + " " + c);
+        graphics.putString(63, 21, "  " + c + c + c + c + c + c + c + c + c + "  " + c);
+        graphics.setForegroundColor(TextColor.ANSI.GREEN_BRIGHT);
+        graphics.putString( 64, 4, "CURRENT SCORE", SGR.BOLD);
+        graphics.putString( 66, 5, Symbols.TRIANGLE_LEFT_POINTING_MEDIUM_BLACK + "");
+        screen.refresh();
     }
+    private Direction getOppositeDirection(Direction direction) {
+        if (direction == Direction.LEFT) {
+            return Direction.RIGHT;
+        }
+        if (direction == Direction.RIGHT) {
+            return Direction.LEFT;
+        }
+        if (direction == Direction.UP) {
+            return Direction.DOWN;
+        }
+        if (direction == Direction.DOWN) {
+            return Direction.UP;
+        }
 
+        return direction;
+    }
+    public boolean checkCollision(){
+        Position start = snake.getHead();
+        for(Boxes p: boxes){
+            if(start.equals(p.getPosition())){
+
+                snake.kill();
+                return true;
+            }
+        }
+
+
+        Position snakeHead = snake.getHead();
+
+        for(int i = snake.getBody().size()-2; i > 0;i--){
+            if(snakeHead.equals(snake.getBody().get(i))){
+                snake.kill();
+                return true;
+
+            }
+        }
+        if(snakeHead.getX() < 0 || snakeHead.getX() > width){
+            snake.kill();
+            return true;
+
+        }
+        if(snakeHead.getY() < 0 || snakeHead.getY() > height){
+            snake.kill();
+            return true;
+        }
+
+
+        return false;
+    }
     public Snake getSnake(){
         return snake;
     }
-    private LinkedList<Apple> createApples(){
+    private void createApples(){
         Random random = new Random();
+        apple = new Apple(new Position (random.nextInt(width - 2) +1, random.nextInt(height - 2) + 1));
 
-        for (int i = 0; i < 7; i++)
-            apples.add(new Apple(new Position (random.nextInt(width - 2) +1, random.nextInt(height - 2) + 1)));
 
-        return apples;
+
+    }
+    public void isAteApple(){
+        Position start = snake.getHead();
+        if(start.equals(apple.getPosition())){
+            snake.getBody().add(snake.whereTo());
+            snake.setAte(true);
+        }
     }
 
-
-    public LinkedList<Apple> getApples() {
-        return apples;
+    public Apple getApples() {
+        return apple;
     }
     private LinkedList<Boxes> createBoxes(){
         Random random = new Random();
@@ -57,36 +165,38 @@ public class Arena {
         return boxes;
     }
 
-    private int width,height;
-    private LinkedList<Apple> apples = new LinkedList<Apple>();
-    private LinkedList<Boxes> boxes = new LinkedList<Boxes>();
-    private Snake snake;
-
-    public void processKey(KeyStroke key) throws IOException {
-        switch (key.getKeyType()) {
-            case ArrowUp:
-                snake.setDirection(Direction.UP);
-                snake.move();
-                break;
-            case ArrowDown:
-                snake.setDirection(Direction.DOWN);
-                snake.move();
-                break;
-            case ArrowLeft:
-                snake.setDirection(Direction.LEFT);
-                snake.move();
-                break;
-
-            case ArrowRight:
-                snake.setDirection(Direction.RIGHT);
-                snake.move();
 
 
-                break;
-            /*case EOF:
-                play = false;
-                break;*/
+
+    public void tick()  {
+        snake.move();
+        if(checkCollision()) return;
+        isAteApple();
+        if(snake.getAte()){
+            createApples();
+            SCORE++;
+        }
+        snake.setAte(false);
+
+    }
+    public void run(){
+        while (true){
+            snake.move();
         }
     }
+    /*public void updateArena(Screen screen) throws IOException {
+        snake.move();
+        isAteApple();
+        if(snake.getAte()) {createApples();SCORE++;}
+        snake.setAte(false);
+        if(!checkCollision()) paintArena(screen);
+
+
+
+
+
+
+
+    }*/
 }
 
--- SnakeLDTS/src/main/java/Game.java
+++ SnakeLDTS/src/main/java/Game.java
@@ -1,37 +1,101 @@
 import com.googlecode.lanterna.*;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
-import java.awt.*;
 import java.io.IOException;
 
 public class Game {
-    public Game(Arena arena,Screen screen){
-        this.arena = arena;
+
+    private final static int GAME_SPEED_1 = 90;
+    private final static int GAME_SPEED_2 = 75;
+    private final static int GAME_SPEED_3 = 60;
+    private final static int GAME_SPEED_4 = 45;
+    private final static int GAME_SPEED_5 = 35;
+    private Snake snake;
+
+    public Game(Screen screen){
+
         this.screen = screen;
+        snake = new Snake(30,15,Direction.RIGHT);
+        this.arena = new Arena(60,24,snake);
     }
-    public void  run() throws IOException {
-        while(true){
-            KeyStroke key;
-            screen.clear();
-            arena.paintArena(screen.newTextGraphics());
-            screen.refresh();
-            key = screen.readInput();
-            arena.processKey(key);
-            arena.getSnake().isAteApple(arena.getApples());
+    private void processKey() throws IOException {
+        KeyStroke key = screen.pollInput();
+        if (key == null) {
+            return;
+        }
+        if (key.getCharacter() != null) {
+            return;
+        }
+        if(validKey(key)) {
+            switch (key.getKeyType()) {
+                case ArrowUp -> snake.setDirection(Direction.UP);
+                case ArrowDown -> snake.setDirection(Direction.DOWN);
+                case ArrowLeft -> snake.setDirection(Direction.LEFT);
+                case ArrowRight -> snake.setDirection(Direction.RIGHT);
+            }
+
+        }
+    }
+    private boolean validKey(KeyStroke key){
+        if(key.getKeyType() == KeyType.ArrowUp && snake.getDirection() == Direction.DOWN){
+            return false;
+        }else if(key.getKeyType() == KeyType.ArrowDown && snake.getDirection() == Direction.UP){
+            return false;
+        }else if(key.getKeyType() == KeyType.ArrowLeft && snake.getDirection() == Direction.RIGHT){
+            return false;
+        }else if( key.getKeyType() == KeyType.ArrowRight && snake.getDirection() == Direction.LEFT){
+            return false;
+        }
+        return true;
+    }
+    private void tick() throws IOException {
+        processKey();
+        arena.tick();
+        screen.clear();
+        arena.paintArena(screen);
+
+
+    }
+    private void beginTicks() throws IOException, InterruptedException {
+        while (arena.getSnake().isSnakeAlive()) {
+            tick();
+            Thread.sleep(1000L / Arena.TICKS_PER_SECOND);
         }
 
+        System.exit(0);
+    }
+
+    public void  run() throws IOException, InterruptedException {
+        beginTicks();
+        /*while(arena.getSnake().isSnakeAlive()){
+
+            KeyStroke key;
+            key = screen.readInput();
+            arena.processKey(key);
+            arena.paintArena(screen);
+            if(counter % GAME_SPEED_1 == 0){
+                arena.tick();
+            }
+
+
+            /*arena.paintArena(screen);
+            arena.getSnake().isAteApple(arena.getApples());
+            arena.checkCollision();
+        }*/
 
 
     }
+
     public void setArena(Arena arena){
         this.arena = arena;
     }
-
+    private boolean running = true;
     private Arena arena;
     private Screen screen;
 }
